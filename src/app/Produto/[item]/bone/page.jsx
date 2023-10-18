a8ed1b7ae1b7f15acfc5ed50602ef12737c58cde
Yuri <yurichichedom808@gmail.comi>
2023-10-17 22:16:53 -0300

    criando função

--- src/app/Produto/[item]/bone/page.jsx
+++ src/app/Produto/[item]/bone/page.jsx
@@ -1,2 +1,9 @@
 import Image from "next/image"
 import '../../../components/ScssProdutos/produtos.scss'
+
+export default function Bone({params}) {
+    return(
+        <>
+        </>
+    )
+    }
