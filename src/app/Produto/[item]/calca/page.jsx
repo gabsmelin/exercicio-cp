5d15843bcae7cc0a293496756191ee86733922be
LucasSpineli003 <rm97757@fiap.com.br>
2023-10-17 22:19:52 -0300

    finalizado

--- src/app/Produto/[item]/calca/page.jsx
+++ src/app/Produto/[item]/calca/page.jsx
@@ -2,6 +2,8 @@ import Image from "next/image"
 import '../../../components/ScssProdutos/produtos.scss'
 
 export default function Calca({params}) {
+
+
     return(
         <div className="container-prod">
             <div>
@@ -18,5 +20,5 @@ export default function Calca({params}) {
                 </li>
             </ul>
         </div>
-        )
+    )
 }
