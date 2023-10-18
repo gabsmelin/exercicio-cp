a9651e02e3ae6226d4a20a5b91add086adf7a815
LucasSpineli003 <rm97757@fiap.com.br>
2023-10-17 22:15:33 -0300

    default function criada.

--- src/app/Produto/[item]/calca/page.jsx
+++ src/app/Produto/[item]/calca/page.jsx
@@ -1,2 +1,9 @@
 import Image from "next/image"
 import '../../../components/ScssProdutos/produtos.scss'
+
+export default function Calca({params}) {
+    return(
+        <div className="container-prod">
+        </div>
+        )
+}
