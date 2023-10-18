a00cdfa3768c2e4e3bf58463ba9f1c64ba156ece
gabsmelin <rm550202@fiap.com>
2023-10-17 22:26:23 -0300

    página de tênis feita e componente de cabecalho arrumado.

--- src/app/components/Cabecalho/page.jsx
+++ src/app/components/Cabecalho/page.jsx
@@ -5,11 +5,11 @@ export default function Cabecalho() {
     return (
         <div className="container-cab">
             <p><Link href="/">Home</Link></p>
-            <p><Link href="/produto/item/calca">Calças</Link></p>
-            <p><Link  href="/produto/item/oculos">Ôculos</Link></p>
-            <p><Link  href="/produto/item/camisa">Camisetas</Link></p>
-            <p><Link  href="/produto/item/tenis">Sapato</Link></p>
-            <p><Link  href="/produto/item/bone">Boné</Link></p>
+            <p><Link href="/Produto/item/calca">Calças</Link></p>
+            <p><Link  href="/Produto/item/oculos">Ôculos</Link></p>
+            <p><Link  href="/Produto/item/camisa">Camisetas</Link></p>
+            <p><Link  href="/Produto/item/tenis">Sapato</Link></p>
+            <p><Link  href="/Produto/item/bone">Boné</Link></p>
         </div>
     )
   }
