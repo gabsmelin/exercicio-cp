2a51d0175315a56d14c20b939923f501328f7364
LucasSpineli003 <rm97757@fiap.com.br>
2023-10-17 22:16:02 -0300

    alterações

--- src/app/Produto/[item]/calca/page.jsx
+++ src/app/Produto/[item]/calca/page.jsx
@@ -4,6 +4,10 @@ import '../../../components/ScssProdutos/produtos.scss'
 export default function Calca({params}) {
     return(
         <div className="container-prod">
+            <div>
+                <h1>CALÇAS</h1>
+                <h5>AS CALÇAS MAIS BONITAS DA QUEBRADA VOCÊ SÓ ENCONTRA AQUI!</h5>
+            </div>
         </div>
         )
 }
