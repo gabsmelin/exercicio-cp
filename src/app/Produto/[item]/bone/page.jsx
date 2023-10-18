295cbe35b833d654db27f0891af2e99f01aff3ca
Yuri <yurichichedom808@gmail.comi>
2023-10-17 22:18:30 -0300

    Conteudo do anuncio bone criado

--- src/app/Produto/[item]/bone/page.jsx
+++ src/app/Produto/[item]/bone/page.jsx
@@ -8,6 +8,15 @@ export default function Bone({params}) {
                 <h1>BONÉ</h1>
                 <h5>AS BONÉ MAIS BONITAS DA QUEBRADA VOCÊ SÓ ENCONTRA AQUI!</h5>
             </div>
+            <ul>
+                <li>
+                    <Image src="/img/bone.jpg" alt="Boné." width={350} height={350}/>
+                    <p>Boné</p>
+                    <p>R$199,00</p>
+                    <p>Boné da nike na cor branca com logo preta.</p>
+                    <button>Esgotado.</button>
+                </li>
+            </ul>
         </div>
     )
 }
