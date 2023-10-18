00b0d3e81140296bcd9bad7f6f10c8ca2b24426b
Yuri <yurichichedom808@gmail.comi>
2023-10-17 22:17:59 -0300

    head do anuncio bone criado

--- src/app/Produto/[item]/bone/page.jsx
+++ src/app/Produto/[item]/bone/page.jsx
@@ -3,7 +3,11 @@ import '../../../components/ScssProdutos/produtos.scss'
 
 export default function Bone({params}) {
     return(
-        <>
-        </>
+        <div className="container-prod">
+            <div>
+                <h1>BONÉ</h1>
+                <h5>AS BONÉ MAIS BONITAS DA QUEBRADA VOCÊ SÓ ENCONTRA AQUI!</h5>
+            </div>
+        </div>
     )
-    }
+}
